cb45e3d85f695a73cb1200030a4bb8b6754f1eb4
Aurimas Valancius <aurimas.valancius@eneba.com>
2023-01-11 19:47:44 +0200

    Added clone function.

--- OOP/magic/Magic.php
+++ OOP/magic/Magic.php
@@ -4,6 +4,7 @@ class Magic {
 
     private string $name;
 
+    private ?string $description = "";
     //kvieciamas objekto sukūrimo metu
     public function __construct(string $name) {
         $this->name = $name;
@@ -14,8 +15,19 @@ class Magic {
         echo "VYKDAU KAIP FUNKCIJĄ";
     }
 
+    public function __sleep(): array {
+        return [
+            'description'
+        ];
+    }
     public function __toString(): string
     {
         return "Užsetintas pavadinmas: " . $this->name;
+    }
+
+    //vykdo klonavimo metu
+    public function __clone()
+    {
+        $this->name = $this->name . ' (Clone)';
     }
 }
--- OOP/magic/index.php
+++ OOP/magic/index.php
@@ -5,3 +5,14 @@ include_once "Magic.php";
 $magic = new Magic('Avada kedavra'); //vykdomas construct
 var_dump((string)$magic); // vykdomsa toString
 $magic(); //vykdom kai funkcija
+
+//Konvertuoja i eilute su visais properciais kvieciamas __serialize pagal key ir vertas, kvieciams __sleep pagal key
+$objektoEilute = serialize($magic);
+echo ($objektoEilute);
+
+//funkcija kuri atkonvertuoja is eilutes i objekta.
+$object = unserialize($objektoEilute);
+var_dump($object);
+
+$naujasKlonuotas = clone $magic;
+var_dump($naujasKlonuotas);
