0d742bf89456a778e37da34018aaaa68fb4bed92
Aurimas Valancius <aurimas.valancius@eneba.com>
2023-01-12 18:15:39 +0200

    Added new lines for magic ticket.

--- OOP/magic/task/index.php
+++ OOP/magic/task/index.php
@@ -4,12 +4,13 @@ include_once "Order.php";
 
 $order1 = new Order("emai@email.com", 4, Order::STATE_IN_PROGRESS);
 
-echo $order1;
+echo $order1 . PHP_EOL;
 
 $orderClone = clone $order1;
 
-echo $orderClone;
+echo $orderClone . PHP_EOL;
 
 file_put_contents('serialize.txt', serialize($orderClone));
 $content = file_get_contents('serialize.txt');
 $newObject = unserialize($content);
+var_dump($newObject);
